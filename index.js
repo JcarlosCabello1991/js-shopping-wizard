42118ee9fe2a9a0ced53365ded0f99dd33751634
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-04-08 17:40:31 +0200

    Add basic function checkInputs() on profile page

--- index.js
+++ index.js
@@ -32,3 +32,38 @@ function toProfile(){
     document.getElementsByClassName("buttons-form-profile").item(0).style.visibility = "visible";
     document.getElementsByClassName("step-journey-circle").item(0).style.backgroundColor = "black";
 }
+
+
+// Functions for profile page
+const nextBtn = document.getElementById('nextButton');
+nextBtn.addEventListener('click', checkInputs);
+
+// Function check inputs on profile page for any value. 
+function checkInputs () {
+    const userName = document.getElementById('userName').value;
+    const pw = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+    const pwConfirm = document.getElementById('pwConfirm').value;
+
+    if (!userName) {
+        const userNameMsg = document.querySelector('#userName + span');
+        userNameMsg.textContent = 'Username cannot be empty.';
+        userNameMsg.classList.add('profile-error');
+    }
+    if (!email) {
+        const emailMsg = document.querySelector('#email + span');
+        emailMsg.textContent = 'Email cannot be empty.';
+        emailMsg.classList.add('profile-error');
+    }
+    if (!pw)  {
+        const pwMsg = document.querySelector('#password + span');
+        pwMsg.textContent = 'Password cannot be empty.';
+        pwMsg.classList.add('profile-error');
+    }
+    if (!pwConfirm) {
+       const pwConfirmMsg = document.querySelector('#pwConfirm + span');
+       pwConfirmMsg.textContent = 'Please confirm your password.'
+       pwConfirmMsg.classList.add('profile-error');
+    }
+}
+
