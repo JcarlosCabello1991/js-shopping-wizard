5153c7a7b502c791bfa0a863a9332996f4f73c0c
marcelbbarreiro <marcelbarreiro@gmail.com>
2022-04-19 15:49:37 +0200

    Marcel

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ function buyNowPressed(){
         document.getElementById("error-checkedButton").classList.add("profile-error");
         document.getElementById("error-checkedButton").textContent = "Please accept the terms and conditions";
     }else{
-        document.getElementsByClassName("container-order").item(0).style.display = "flex";
+        document.getElementsByClassName("container-order").item(0).style.display = "flow-root";
         document.getElementsByClassName("container-Purchase").item(0).style.display = "none";
         document.getElementById("error-checkedButton").classList.remove("profile-error");
         document.getElementById("error-checkedButton").textContent = "";
